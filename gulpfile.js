dc20de317b4c4e5a77bde3652c86e64bed9b527d
abkendal <kendal.adam@mail.com>
2015-10-05 16:49:34 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('styles', function(){
+	return gulp.src('styles/*.scss')
+			.pipe(sass().on('error',sass.logError))
+			.pipe(concat('style.css'))
+			.pipe(gulp.dest('styles/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('styles/*.scss', ['styles']);
+});
+
+gulp.task('default', ['watch']);
